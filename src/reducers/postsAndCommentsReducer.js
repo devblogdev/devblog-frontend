4fb8183943af24d142cada57385f7a55c1197e50
Luis Martinez <luis_mmartinez@live.com>
2021-07-29 09:10:02 -0400

    defined placeholder ADD_LIKE and REMOVE_LIKE actions in postsAndCommentsReducer

--- src/reducers/postsAndCommentsReducer.js
+++ src/reducers/postsAndCommentsReducer.js
@@ -13,12 +13,41 @@ export default function postsAndCommentsReducer(
                     loading: true,
                     message: "Loading posts..."
                 }
-            case 'FETCH_STUDIES':
+            case 'FETCH_POSTS':
                 return {
                     ...state,
-                    studies: action.studies,
+                    posts: action.posts,
                     loading: false,
                     message: ""
+                }
+            
+            case 'ADD_LIKE':
+                const user = "some user"
+                const postId= "someId"
+                const post= state.posts.find(post => post.id === postId)
+                const postIndex = state.posts.findIndex( post => post.id === postId )
+                const newLike = {userId: user}
+                post = [...post, ...post.likes, newLike]
+                return {
+                    ...state,
+                        posts: [
+                            state.posts.slice(0, postIndex), post, state.posts(postIndex+1)
+                        ]
+                }
+            case 'REMOVE_LIKE':
+                const postID= "someId"
+                // const retrieveIdandIndex = (object) => state.posts.find( (post, index) => {post.id === postId})
+                const user = "some user"
+                const postIndex = state.posts.findIndex( post => post.id === postId )
+                const post = "hi"
+                const likeIndex = post.likes.findIndex(like => like.userId === user)
+                const postLikes = [ post.likes.slice(0, likeIndex), post.likes.slice(likeIndex+1) ] 
+                post.likes = postLikes
+                return {
+                    ...state, 
+                        posts: [
+                            state.posts.slice(0, postIndex), post, state.posts(postIndex+1)
+                        ]
                 }
             default:
                 return state
