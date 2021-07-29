03a17be6254797451c8e3a982a1339919466b574
Luis Martinez <luis_mmartinez@live.com>
2021-07-29 09:52:15 -0400

    defined a placeholder for ManageLogin container

---
+++ src/containers/ManageLogin.js
@@ -0,0 +1,10 @@
+
+
+const ManageLogin = (props) => {
+    if (props.match.url === '/signup') {
+        return <div>the signup component</div>
+    } 
+    return <div>the signup component</div>
+}
+
+export default ManageLogin
