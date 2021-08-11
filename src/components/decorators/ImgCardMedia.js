bc9f20ae9566d103ff2797bc20d88a7a3cf217ce
Luis Martinez <luis_mmartinez@live.com>
2021-08-11 19:43:27 -0400

    refactored home component to render a Main post, 2 featured posts, and a mix of database and API posts

---
+++ src/components/decorators/ImgCardMedia.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+
+export default function ImgCardMedia(props) {
+
+  const useStyles = () => makeStyles({
+    root: {
+      width: "100%",
+      media: {
+          height: 540,
+      },
+    },
+  });
+   
+  const classes = useStyles();
+  const { post } = props
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt= {post.title}
+          height= {props.imageHeight}
+          image= {post.images[0].url}
+          title= {post.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {post.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {post.body}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Continue reading...
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import PostList from '../components/posts/PostList';
+// import PostList from '../components/posts/PostList';
+import { Link } from 'react-router-dom'
+import ImgCardMedia from '../components/decorators/ImgCardMedia'
+import ExternalImgCardMedia from '../components/decorators/ExternalImgCardMedia'
 
 // MAIN COMPONENT; FUNCTIONAL COMPONENT
 const Home = (props) => {
@@ -14,11 +17,48 @@ const Home = (props) => {
         }
     })
 
+    const published = props.posts.filter( post => post.status === "published")
+
+    const mainPost = useCallback(() => {
+        if (published.length > 0) {
+          return  <Link to={`/posts/${published[0].id}`} style={{ textDecoration: 'none' }}>
+                    <ImgCardMedia post={published[0] } className="mainPost" imageHeight={"290"}/>
+                  </Link>
+        }
+    },[published])
+     
+    const featuredPosts = published.slice(1,3).map ( (post, index) => {
+        return <Link key={index} to={`/posts/${post.id}`} style={{ textDecoration: 'none' }} >
+                    <ImgCardMedia post = {post} height= {100} imageHeight={"200"} />
+                </Link>
+    })
+
+    const remainderPosts = published.slice(3).concat(props.posts.filter( post => post.coming_from !== "database")).map( (post,index) => {
+        if (post.coming_from === "database") {
+            return <Link key={index} to={`/posts/${post.id}`} style={{ textDecoration: 'none' }} >
+                      <ImgCardMedia post = {post} height= {100} imageHeight={"140"} />
+                   </Link>
+        }
+        return <a key={-index} href={post.url} target="_blank" rel="noreferrer noopener" style={{ textDecoration: 'none' }} >
+                    <ExternalImgCardMedia key={index} post = {post} />
+               </a>
+        })
+
     return (
-        <React.Fragment>
-            <h1>Welcome to DevBlog</h1>
-            <PostList posts ={props.posts} {...props} />
-        </React.Fragment>
+        <div className='home'>
+            
+            <div className="mainPost">
+                {mainPost()} 
+            </div>
+            <div className="featuredPosts">
+                {featuredPosts}
+            </div>
+            <div className="remainderPosts">
+                {remainderPosts}
+            </div>
+            {/* <h1>Welcome to DevBlog</h1> */}
+            {/* <PostList posts ={props.posts} {...props} />  */}
+         </div>
     )
 }
 
