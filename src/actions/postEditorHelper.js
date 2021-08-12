bffc54856be2bd921927dd284ba6f5bbf3541ef7
Luis Martinez <luis_mmartinez@live.com>
2021-08-12 09:35:26 -0400

    changed regular expression for extracting post title; expression giving errors

--- src/actions/postEditorHelper.js
+++ src/actions/postEditorHelper.js
@@ -19,7 +19,7 @@
 // let alternative = /(?:\/)([^#]+)(?=#*)/
 export function extractTitle(data) {
     // This regular expression causes errors in Safari browser
-    let result = data.match(/(?:\>).*?(?=<)/g);
+    let result = data.match(/(?:>).*?(?=<)/g);
     console.log(result);
     return result
 }
