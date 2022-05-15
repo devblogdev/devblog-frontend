afd4b6af157e9b3b4c0a1b897799886f217d2118
Luis Martinez <luis_mmartinez@live.com>
2022-05-15 01:06:33 -0400

    created the S3Client class and scaffolds for anticipated instance methods

--- src/aws-s3/aws-s3.js
+++ src/aws-s3/aws-s3.js
@@ -1,1 +1,36 @@
+const hhtp2 = require ('http2');
+
 "Creating a Node Js AWS-S2 Software Development Kit"
+class S3Client {
+
+    constructor (
+        {
+            hostUrl, 
+            apiKey
+        }
+        // call initialize emthod during instantiation
+    )
+    // Perform sanity check for instance constructor properties
+    initialize(){
+        // Connect to S3
+        // Use httpw mdoule + XMLS
+    }
+
+    // CRUD ACTIONS
+    get(){
+
+    }
+
+    upload(paplod){
+        // Sanity check payload
+    }
+
+    update(payload){
+
+    }
+
+    delete(payload){
+
+    }
+
+}
