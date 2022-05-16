3b48cb2b5f004b4ed67312ff76dcb7d93123bf6b
Luis Martinez <luis_mmartinez@live.com>
2022-05-16 00:41:30 -0400

    completed XML request function

--- src/aws-s3/aws-s3.js
+++ src/aws-s3/aws-s3.js
@@ -1,6 +1,6 @@
 class S3Client {
 
-    static baseUrl = "aws-s3-baseUrl"
+    static baseUrl = ""
 
     constructor ({ bucketName, region, accessKeyId, secretAccessKey }) {
         this.config = {};
@@ -8,9 +8,25 @@ class S3Client {
         this.config["region"] = region
         this.config["accessKeyId"] = accessKeyId
         this.config["secretAccessKey"] = secretAccessKey
+
+        S3Client.baseUrl = 'https://' + bucketName + '.' + region + '.amazonaws.com/'
     }
 
-    request(method, requestUrl) {
+    request(headers, path, method, queryStringObject, payload, callback) {
+
+        let requestUrl = path + '?';
+        let counter = 0;
+        for(let queryKey in queryStringObject){
+            if(queryStringObject.hasOwnProperty(queryKey)){
+                counter++;
+                if(counte > 1){
+                    requestUrl+= '&';
+                }
+                requestUrl+= queryKey + '=' +queryStringObject[queryKey];
+            }
+        }
+
+
         let xhr = new XMLHttpRequest();
         xhr.open(method, requestUrl, true);
         xhr.setRequestHeader('Content-type', 'application/json');
@@ -18,6 +34,25 @@ class S3Client {
             xhr.setRequestHeader(configKey, this.config[configKey]);
         }
         
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == XMLHttpRequest.DONE) {
+                let statusCode = xhr.status;
+                let responseReturned = xhr.responseText;
+
+                if(callback){
+                    try{
+                        let parsedResponse = JSON.parse(responseReturned);
+                        callback(statusCode, parsedResponse);
+                    } catch(e){
+                        callback(statusCode, false);
+                    }
+                }
+
+            }
+        }
+
+        let payloadString = JSON.stringify(payload);
+        xhr.send(payloadString);
     }
     
     // Perform sanity check for instance constructor properties
