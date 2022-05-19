d5e6c83de3d9ae953741058899b3deb9bc154afe
Luis Martinez <luis_mmartinez@live.com>
2022-05-18 22:45:35 -0400

    collected information from AWS S3 official documentation for fields required to make post request to upload a file to S3

--- src/aws-s3/aws-s3.js
+++ src/aws-s3/aws-s3.js
@@ -1,3 +1,4 @@
+
 class S3Client {
 
     static baseUrl = ""
@@ -51,8 +52,51 @@ class S3Client {
             }
         }
 
-        // let payloadString = JSON.stringify(payload);
-        let payloadString = payload;
+        let payloadString = JSON.stringify(payload);
+        // let payloadString = Buffer.from(payload, 'base64');
+
+        // Create a form to send to AWS S3
+        // let form = new FormData();
+        
+        // AWS S3 Documentation: https://docs.aws.amazon.com/AmazonS3/latest/API/Welcome.html
+        // Form fields for HTTP POST request to AWS S3
+        /*
+        * policy    (Base64-eoncded security policy)
+        * x-amz-algorithm   (for AWS Signature Version 4 use AWS4-HMAC-SHA256)        
+        * x-amz-credential  (has the form: <your-access-key-id>/<date>/<awsregion>/<aws-service>/aws4_request)
+        * x-amz-date    (date in ISO8601 format: 20130728T000000Z)
+        * x-amz-signature   (the HMAC-SHA256 hash of the security policy)
+        * 
+        * Calculating a Signature
+        * 
+        * POST FORM FIELDS
+        * 
+        * AWSAccessKeyId    
+        * acl   (access control list: valid values: private | public-read | public-read-write | aws-exec-read | authenticated-read | bucket-owner-read | bucket-owner-full-control); use "public-read"
+        * file  (file to upload; must be last field in form)
+        * key   (used to assign name to file in s3)
+        * x-amz-server-side-encryption  (server-side encryption algorithm; valid values: aws:kms, AES256; use "AES256")
+        * 
+        * SIGNATURES
+        * 
+        *   Signature = Hex(HMAC-SHA256(SigningKey, StringToSign))
+        *       
+        *       SigningKey 
+        *       DateKey = HMAC-SHA256("AWS4" + "<SecretAccessKey>", <yyymmdd> )
+        *       DateRegionKey = HMAC-SHA256(DateKey, "<aws-region>")
+        *       DateRegionServiceKey = HMAC-SHA256(DateRegionKey, "<aws-service>")
+        *       SigningKey = HMAC-SHA256(DateServiceKey, "aws4_request")
+        * 
+        *       StringToSign
+        *       Base-64-encoded security policy
+        * 
+        */          
+
+
+
+
+
+        console.log(payload);
         xhr.send(payloadString);
     }
     
