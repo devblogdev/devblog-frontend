13c0ad00947caaebbfe074b7e8ccc4879777c9a0
Luis Martinez <luis_mmartinez@live.com>
2022-05-21 19:19:16 -0400

    created bucket policy, same as stringToSign for HTTP Post requests

--- src/aws-s3/aws-s3.js
+++ src/aws-s3/aws-s3.js
@@ -1,4 +1,9 @@
-
+// Documentation: 
+// This software-development-kid performs 'browser-based uploads to Amazon S3'.
+// For official documention on the above process, refer to the "Amazon Simple Storage Service API Reference",
+// section "Authenticating Requests in Browser-Based Uploads Using POST (AWS Signature Version 4)", 
+// subsections "Broswer-Based uploads Using HTTP-POST", "Calculating a Signature", "POST Policy", 
+// and "Example: Browser-Based Upload using HTTP POST (Using AWS Signature Version 4)"
 
 class S3Client {
     static crypto = require('crypto');
@@ -97,47 +102,39 @@ class S3Client {
         */          
 
 
-        // Example Signature Calculation
-        let object = {
-            "AWSAccessKeyId": "AKIAIOSFODNN7EXAMPLE",
-            "AWSSecretAccessKey": "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-            "region": "us-east-1",
-            "X-Amz-Algorithm": "AWS4-HMAC-SHA256",
-            "aws-service": "s3"
-        };
+        // Creating the stringToSign using the bucket Policy
 
-        let stringToSign = `AWS4-HMAC-SHA256\n20130524T000000Z\n20130524/us-east-1/s3/aws4_request\n3bfa292879f6447bbcda7001decf97f4a54dc650c8942174ae0a9121cf58ad04`
+        const tenMinutes = 6e5;
+        const isoDate = (new Date(new Date().getTime() + tenMinutes)).toISOString();
+        const date = isoDate.split("T")[0].split("-").join("");  // yyyymmdd
+        const formattedIso = isoDate.split("-").join("").split(":").join("").split(".").join("");
 
-        // Calculating the SigningKey
-        let c = S3Client.crypto;
-        // c ( algorithm, key, ..rest(string))
-        const dateKey = c.createHmac('sha256', "AWS4" + object.AWSSecretAccessKey).update("20130524").digest('hex');
-        const dateRegionKey = c.createHmac('sha256', dateKey).update(object['region']);
-        const dateRegionServiceKey = c.createHmac('sha256', dateRegionKey).update('s3');
-        const signingKey = c.createHmac('sha256', dateRegionServiceKey).update('aws4_request');
-        // signing key = HMAC-SHA256(HMAC-SHA256(HMAC-SHA256(HMAC-SHA256("AWS4" + "<YourSecretAccessKey>","20130524"),"us-east-1"),"s3"),"aws4_request")
-
-        const signature = c.createHmac('sha256', signingKey).update(stringToSign).digest('hex');
-
-        let d = S3Client.crypto2;
-        const m = d.HmacSHA256("message", "key");
-        const n  = d.HmacSHA256("message", "key");
-        const j = c.createHmac('sha256', "key").update("message")
-        const k = c.createHmac('sha256', "key").update("message")
-        // console.log(m);
-        // console.log(n);
-        // console.log(j);
-        // console.log(k);
-        // console.log(new Date())
-        // console.log(signature);
-        // xhr.send(payloadString);
-
-
+        // Note: The optional "encoding" parameter from "Buffer.from(string[,enconding])" defaults to utf8
         const policy = Buffer.from(
             JSON.stringify(
                 { 
-                    "expiration": "2015-12-30T12:00:00.000Z",
-                    "conditions": [
+                    expiration: isoDate,
+                    conditions: [
+                        {"bucket": this.config.bucketName},
+                        ["starts-with", "$key", ""],
+                        {"acl": "public-read"},
+                        ["starts-with", "$Content-Type", ""],
+                        {"x-amz-meta-uuid": "14365123651274"},
+                        {"x-amz-server-side-encryption": "AES256"},
+                        ["starts-with", "$x-amz-meta-tag", ""],
+                        {"x-amz-credential": `${this.config.accessKeyId}/${date}/${this.config.region}/s3/aws4_request`},
+                        {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
+                        {"x-amz-date": formattedIso }
+                    ] 
+                }
+            )
+        ).toString('base64').replaceAll(/[$\n\r]/g, "")
+
+        const policy2 = new Buffer(
+            JSON.stringify(
+                { 
+                    expiration: "2015-12-30T12:00:00.000Z",
+                    conditions: [
                         {"bucket": "sigv4examplebucket"},
                         ["starts-with", "$key", "user/user1/"],
                         {"acl": "public-read"},
@@ -155,17 +152,49 @@ class S3Client {
         ).toString('base64').replace(/\n|\r/, "")
 
 
-        console.log(policy)
+        // Calculating the SigningKey
+
+        let c = S3Client.crypto;
+        // c ( algorithm, key, ..rest(string))
+        const dateKey = c.createHmac('sha256', "AWS4" + this.config.secretAccessKey).update(date).digest();
+        const dateRegionKey = c.createHmac('sha256', dateKey).update(this.config.region).digest();
+        const dateRegionServiceKey = c.createHmac('sha256', dateRegionKey).update('s3').digest();
+        const signingKey = c.createHmac('sha256', dateRegionServiceKey).update('aws4_request').digest();
+        // signing key = HMAC-SHA256(HMAC-SHA256(HMAC-SHA256(HMAC-SHA256("AWS4" + "<YourSecretAccessKey>","20130524"),"us-east-1"),"s3"),"aws4_request")
+        const signature = c.createHmac('sha256', signingKey).update(policy).digest('hex');
+                
+        let d = S3Client.crypto2;
+        // d.HmacSHA256("meesage", "key")
+        const m = d.HmacSHA256(date, "AWS4" + this.config.secretAccessKey);
+        const n  = d.HmacSHA256(this.config.region, m);
+        const t = d.HmacSHA256("s3", n);
+        const p = d.HmacSHA256("aws4_request", t);
+
+        const signature2 = d.HmacSHA256(policy, p).toString(d.enc.Hex);
 
 
+        // console.log(signature, signature2)
+    
+        const w = d.HmacSHA256("message", "key")
+        const j = c.createHmac('sha256', "key").update("message")
+        console.log(j, w);
+        debugger
+        // const k = c.createHmac('sha256', "key").update("message")
+        // console.log(m);
+        // console.log(n);
+        // console.log(j);
+        // console.log(k);
+        // console.log(new Date())
+        // console.log(signature);
+        // xhr.send(payloadString);
 
     }
     
     // Perform sanity check for instance constructor properties
     uploadFile(file) {
         this.request(this.config, S3Client.baseUrl, 'POST', {}, file, function(statusCode, responseReturned){
-            console.log(statusCode);
-            console.log(responseReturned);
+            // console.log(statusCode);
+            // console.log(responseReturned);
         });
     }
 
