f6eb4dfbe35a6cb6bdb73b7e4a2be61f8b1a39c1
Luis Martinez <luis_mmartinez@live.com>
2023-04-09 14:29:31 -0400

    Move repo to new DevBlog github organization; apply Prettier formatting to Login.js and DevBlogLogoFrame.js

--- src/components/logo/DevBlogLogoFrame.js
+++ src/components/logo/DevBlogLogoFrame.js
@@ -1,41 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export default function DevBlogLogoFrame( 
-    { 
-        child, 
-        backgroundMajor, 
-        backgroundMinor, 
-        border, 
-        shape, 
-        height, 
-        width 
-    }) {
-    return (
-        <div 
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                backgroundColor: backgroundMajor,
-                width: '90%',
-                margin: 'auto',
-            }}
-        >
-            <div  
-                style= {{
-                    boxSizing: 'border-box',
-                    width: width || '220px',
-                    height: height || '140px',
-                    borderRadius: shape,
-                    fontSize: '20px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    border: border,
-                    backgroundColor: backgroundMinor,
-                }}
-            >
-                {child}  
-            </div>       
-        </div>
-    )
+export default function DevBlogLogoFrame({
+  child,
+  backgroundMajor,
+  backgroundMinor,
+  border,
+  shape,
+  height,
+  width,
+}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: backgroundMajor,
+        width: "90%",
+        margin: "auto",
+      }}
+    >
+      <div
+        style={{
+          boxSizing: "border-box",
+          width: width || "220px",
+          height: height || "140px",
+          borderRadius: shape,
+          fontSize: "20px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          border: border,
+          backgroundColor: backgroundMinor,
+        }}
+      >
+        {child}
+      </div>
+    </div>
+  );
 }
--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,40 +1,38 @@
-
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 // MATERIAL UI DEPENDENCIES
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 // APP DEPENDENCIES
-import { NavLink } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { createOrLoginUser } from '../../actions/userActions'
-import DevBlogLogoWhiteColor from '../logo/DevBlogLogoWhiteColor';
-import DevBlogLogoFrame from '../logo/DevBlogLogoFrame';
-import { grey } from '@material-ui/core/colors'
-import GoogleLogin from 'react-google-login';
-import { googleUser } from './googleOAuth/googleUser';
-import axios from 'axios';
-import { ModalContext } from '../modal/ModalContext';
-
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { createOrLoginUser } from "../../actions/userActions";
+import DevBlogLogoWhiteColor from "../logo/DevBlogLogoWhiteColor";
+import DevBlogLogoFrame from "../logo/DevBlogLogoFrame";
+import { grey } from "@material-ui/core/colors";
+import GoogleLogin from "react-google-login";
+import { googleUser } from "./googleOAuth/googleUser";
+import axios from "axios";
+import { ModalContext } from "../modal/ModalContext";
 
 // MATERIAL UI FUNCTION
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://devblog.dev">
         DevBlog
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -43,16 +41,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(5),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -62,79 +60,80 @@ const useStyles = makeStyles((theme) => ({
 
 // MAIN: FUCNTIONAL COMPONENT; RENDERS A LOGIN FORM
 export default function Login(props) {
-
   const classes = useStyles();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [displayErrors, setDisplayErrors] = useState(false)
-  const dispatch = useDispatch()
+  const [displayErrors, setDisplayErrors] = useState(false);
+  const dispatch = useDispatch();
 
-  const { retrieveModalState } = useContext(ModalContext)
-
+  const { retrieveModalState } = useContext(ModalContext);
 
   const fieldValidator = (field, fieldName) => {
-    if (field.trim() === '') {
-      return (
-          <p className="errorField">{fieldName} can't be blank</p>
-      )
-    } 
-    return null
-  }
+    if (field.trim() === "") {
+      return <p className="errorField">{fieldName} can't be blank</p>;
+    }
+    return null;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (fieldValidator(email) || fieldValidator(password) ) {
-      setDisplayErrors(true)
-    } 
-    else {
-      const endpoint = "/api/v1/auth"
-      const userData = {email, password}
-      dispatch(createOrLoginUser(endpoint, userData, props))
-      setEmail("")
-      setPassword("")
-      setDisplayErrors(false)
+    if (fieldValidator(email) || fieldValidator(password)) {
+      setDisplayErrors(true);
+    } else {
+      const endpoint = "/api/v1/auth";
+      const userData = { email, password };
+      dispatch(createOrLoginUser(endpoint, userData, props));
+      setEmail("");
+      setPassword("");
+      setDisplayErrors(false);
     }
-  }
-  
+  };
+
   const responseGoogle = (data) => {
-    if(!data.error){
+    if (!data.error) {
       // for later usage: use this to have the server request an access token for user, thus the server can verify the credentials and logout the uswer if tokens have experied
       // const { code } = data
       // axios.post("/tokens", {code})
       // for later usage
-      const user = googleUser(data)
-      axios.post("/omniauth/google/callback", user)
-        .then( response => {
-          localStorage.setItem('token', response.data.jwt)
+      const user = googleUser(data);
+      axios
+        .post("/omniauth/google/callback", user)
+        .then((response) => {
+          localStorage.setItem("token", response.data.jwt);
           const payload = {
-            user: typeof(response.data.user) === "object" ? response.data.user : JSON.parse(response.data.user)
-          }
-          dispatch({type: 'SET_USER', payload: payload })
-          props.history.push('/')
-          retrieveModalState(["You have been successfully logged in"])
-        }).catch( error => {
-          console.log(error)
-          const errorMessage = error?.response?.data?.message
-          if(errorMessage) {retrieveModalState(errorMessage, 9000);}
-          else retrieveModalState(error.message)
+            user:
+              typeof response.data.user === "object"
+                ? response.data.user
+                : JSON.parse(response.data.user),
+          };
+          dispatch({ type: "SET_USER", payload: payload });
+          props.history.push("/");
+          retrieveModalState(["You have been successfully logged in"]);
         })
-      } else {
-        console.log(data.error)
-      }
-  }
-  
+        .catch((error) => {
+          console.log(error);
+          const errorMessage = error?.response?.data?.message;
+          if (errorMessage) {
+            retrieveModalState(errorMessage, 9000);
+          } else retrieveModalState(error.message);
+        });
+    } else {
+      console.log(data.error);
+    }
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <DevBlogLogoFrame
-          child = { <DevBlogLogoWhiteColor /> }
-          border = "solid 1px"
-          backgroundMinor = { grey[500] }
-          shape = "15px"
-          height = "80px"
-          width = "100px"
+          child={<DevBlogLogoWhiteColor />}
+          border="solid 1px"
+          backgroundMinor={grey[500]}
+          shape="15px"
+          height="80px"
+          width="100px"
         />
         <Typography component="h1" variant="h5">
           DevBlog
@@ -150,8 +149,8 @@ export default function Login(props) {
             name="email"
             autoComplete="email"
             autoFocus
-            value ={email}
-            onChange = { event => setEmail(event.target.value)}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
           {displayErrors && fieldValidator(email, "Email")}
           <TextField
@@ -164,8 +163,8 @@ export default function Login(props) {
             type="password"
             id="password"
             autoComplete="current-password"
-            value ={password}
-            onChange = { event => setPassword(event.target.value)}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
           />
           {displayErrors && fieldValidator(password, "Password")}
           <Button
@@ -177,33 +176,33 @@ export default function Login(props) {
           >
             Login
           </Button>
-          <Grid container justifyContent="flex-end">            
+          <Grid container justifyContent="flex-end">
             <Grid item>
-            Don't have an account?
+              Don't have an account?
               <NavLink to="/signup" variant="body2">
-              {" Sign Up"}
+                {" Sign Up"}
               </NavLink>
             </Grid>
             <Grid item>
               <NavLink to="/password-reset" variant="body2">
-              {"Forgot password?"}
+                {"Forgot password?"}
               </NavLink>
-            </Grid>            
+            </Grid>
           </Grid>
         </form>
       </div>
-      
-      <br/>
-      <hr/>
+
+      <br />
+      <hr />
       {/* Google button */}
       <GoogleLogin
-        clientId= {process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
         buttonText="Login with Google"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn= {false}
-        prompt='consent'
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={false}
+        prompt="consent"
         // responseType='code'
         // accessType='offline'
       />
