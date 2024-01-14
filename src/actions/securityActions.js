1f6813dee01e66d5c15affaafe9099dcb11050ca
Luis Martinez <luis_mmartinez@live.com>
2024-01-13 23:45:41 -0500

    apply prettier

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,35 +1,36 @@
-import axios from 'axios'
+import axios from "axios";
 
-export function authorization(endpoint=null, routerAndModal=null ) {
-    const token = localStorage.getItem('token')    
-    const url = endpoint || '/profile' 
-    const axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${token}`   
+export function authorization(endpoint = null, routerAndModal = null) {
+  const token = localStorage.getItem("token");
+  const url = endpoint || "/profile";
+  const axiosConfig = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  if (token) {
+    return async (dispatch) => {
+      try {
+        const response = await axios.get(`${url}`, axiosConfig);
+        const payload = {
+          user: response.data,
+        };
+        console.log("Authorization function called");
+        dispatch({ type: "SET_USER", payload: payload });
+      } catch (error) {
+        console.log(error);
+        routerAndModal?.history?.push("/");
+        if (routerAndModal?.retrieveModalState) {
+          if (error.response.status === 401) {
+            routerAndModal.retrieveModalState(["Your session has expired"]);
+          }
         }
-    }
-    if(token){
-        return async (dispatch) => {
-            try {
-                const response = await axios.get(`${url}`, axiosConfig)
-                const payload = {
-                    user: response.data
-                }
-                console.log("Authorization function called")
-                dispatch({ type:'SET_USER', payload: payload })  
-            } catch(error) {
-                console.log(error)
-                routerAndModal?.history?.push("/")
-                if(routerAndModal?.retrieveModalState){
-                    if(error.response.status === 401){
-                        routerAndModal.retrieveModalState(["Your session has expired"])
-                    }
-                }
-                dispatch({type: 'LOGOUT_USER'})
-            }     
-        }
-    } return (dispatch) => {
-        dispatch({type: 'LOGOUT_USER'})
-    }
+        dispatch({ type: "LOGOUT_USER" });
+      }
+    };
+  }
+  return (dispatch) => {
+    dispatch({ type: "LOGOUT_USER" });
+  };
 }
